ddbfe5f506bed31991b0b90125d2367f001985ba
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-01-07 23:19:38 -0800

    added get all function

---
+++ config/orm.js
@@ -0,0 +1,13 @@
+const connection = require('./connection');
+
+const orm = {
+    all: (tableInput, cb) => {
+        const queryString = `SELECT * FROM ${tableInput} ;`
+        connection.query(queryString, (err, data) => {
+            if (err) {
+                throw err;
+            }
+            cb(data);
+        })
+    }
+}
