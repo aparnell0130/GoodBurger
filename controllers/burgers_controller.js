df43d7802da76fbf281e30104540da5e803a2898
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-01-08 00:25:01 -0800

    added get request in controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,3 +3,13 @@ const express = require('express');
 const router = express.Router();
 
 const burger = require('../models/burger');
+
+router.get('/', (req, res) => {
+    burger.all((data) => {
+        const handlebars = {
+            burger: data
+        }
+        console.log(handlebars)
+        res.render('index', handlebars)
+    })
+})
