92a6aed0b5457b01dad03dbfce2820f7d420a9ce
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-01-10 18:00:10 -0800

    added controller update/put functions

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -20,4 +20,15 @@ router.post('/api/burgers', (req, res) => {
     })
 })
 
+router.put('/api/burgers/:id', (req, res) => {
+    const condition = `id = ${req.params.id}`
+    burger.update({ devoured: req.body.devoured }, condition, (data) => {
+        if (data.affectedRows === 0) {
+            return res.status(404).end()
+        }
+        else {
+            res.status(200).end()
+        }
+    })
+})
 module.exports = router
