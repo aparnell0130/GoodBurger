26d04e17a9f386ee8c32af54593f0bb8b14e20db
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-01-11 11:33:23 -0800

    added jaws db variable

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,17 @@
 const mysql = require('mysql');
-const util = require('util');
-
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'aaron',
-    password: 'User1234',
-    database: 'burgers_db'
-});
+let connection
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'aaron',
+        password: 'User1234',
+        database: 'burgers_db'
+    })
+};
 
 connection.connect();
-
-connection.query = util.promisify(connection.query)
 
 module.exports = connection
