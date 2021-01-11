c11de00f6449d9a77d1d5e2ba3fda9799add8bb2
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-01-11 11:22:02 -0800

    moved ajax function to public js file

--- db/schema.sql
+++ db/schema.sql
@@ -6,6 +6,6 @@ USE burgers_db;
 
 CREATE TABLE burgers(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    burger_name VARCHAR (45),
+    burger_name VARCHAR (45) NOT NULL ,
     devoured boolean NOT NULL DEFAULT 0
 )
---
+++ public/assets/js/index.js
@@ -0,0 +1,33 @@
+$(function () {
+
+    $('#burgerForm').on('submit', (event) => {
+        event.preventDefault()
+        const newBurger = { burger_name: $('#burgerText').val().trim() }
+        console.log(newBurger)
+        $.ajax('/api/burgers', {
+            type: 'POST',
+            data: newBurger
+        })
+            .then(function () {
+                console.log('created new burger')
+                location.reload()
+            })
+    })
+    $('#eat').on('click', function (event) {
+        const id = $(this).data('id')
+        const eaten = $(this).data('eaten')
+        const ate = {
+            devoured: eaten
+        }
+        console.log(ate, id)
+        $.ajax('api/burgers/' + id, {
+            type: 'PUT',
+            data: ate
+        })
+            .then(function () {
+                console.log('burger has been devoured')
+                location.reload()
+            })
+    })
+
+})
